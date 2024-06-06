0b62bc2888b930ba384f5831ffcb3bc5bdd5f57d
Adam Szustak <as.szustak@gmail.com>
2024-06-06 12:03:36 +0200

    Add controller singleton

--- anki_tutor/llm/helper.py
+++ anki_tutor/llm/helper.py
@@ -1,4 +1,5 @@
 from typing import Final
+from __future__ import annotations
 
 BASE_DIR_FOR_SAVING: Final[str] = "saved_assets"
 
@@ -9,3 +10,36 @@ class SoundWritingError(Exception):
 
 class TextWritingError(Exception):
     """Exception raised when error during saving text file"""
+
+
+class AppController:
+    _instance: AppController | None = None
+
+    def __new__(cls, *args, **kwargs) -> AppController:  # type: ignore
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance._init()
+        else:
+            cls._instance._update(*args, **kwargs)
+        return cls._instance
+
+    def _init(self) -> None:
+        self.wait_for_wakeup: bool = True
+        self.practise_sentences: bool = False
+        self.practise_notes: bool = False
+
+    def _update(
+        self,
+        wait_for_wakeup: bool | None = None,
+        practise_sentences: bool | None = None,
+        practise_notes: bool | None = None,
+    ) -> None:
+        if wait_for_wakeup is not None:
+            self.wait_for_wakeup = wait_for_wakeup
+        if practise_sentences is not None:
+            self.practise_sentences = practise_sentences
+        if practise_notes is not None:
+            self.practise_notes = practise_notes
+
+    def __str__(self) -> str:
+        return f"{self.__class__.__name__} -> wait_for_wakeup: {self.wait_for_wakeup}, practise_sentences: {self.practise_sentences}, practise_notes: {self.practise_notes}"
