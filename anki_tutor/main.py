47d0ba8209a4fef275a7bf7dfa914c8435d1dac7
Adam Szustak <as.szustak@gmail.com>
2024-06-12 12:55:06 +0200

    Add basic logging

--- anki_tutor/main.py
+++ anki_tutor/main.py
@@ -1,4 +1,5 @@
 ### WIP ###
+import logging
 import os
 
 from dotenv import load_dotenv
@@ -15,19 +16,27 @@ from llm import (
 load_dotenv()
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        format="%(asctime)s: %(levelname)s - %(message)s - %(module)s",
+        level=logging.INFO,
+        datefmt="%d-%b-%y %H:%M:%S",
+    )
     db_path: str | None = os.getenv("DB_PATH")
     if not db_path:
         raise NoRequiredVarEnv("DB_PATH")
 
     with SQLiteClient(db_path) as db:
-        print(db.get_random_notes(4))
+        logging.info(db.get_random_notes(4))
 
     ai_api_key: str | None = os.getenv("WHISPER_API_KEY")
     if not ai_api_key:
         raise NoRequiredVarEnv("WHISPER_API_KEY")
 
     stt_client: SpeechToText = SpeechToText(ai_api_key)
+    logging.info("%s initialized", stt_client.__class__.__name__)
     sr_client: SpeechRecognizer = SpeechRecognizer(stt_client)
+    logging.info("%s initialized", sr_client.__class__.__name__)
+    logging.info("Start listening")
     sr_client.start_listening(save_assets=True)
     while True:
         pass
